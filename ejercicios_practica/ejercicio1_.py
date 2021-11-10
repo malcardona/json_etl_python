898074e1ff85a2f0515322a0aee5c53e4f764a70
Alejandro L. Cardona <acardona@unc.edu.ar>
2021-11-09 21:06:23 -0300

    Update ejercicio1_.py

--- ejercicios_practica/ejercicio1_.py
+++ ejercicios_practica/ejercicio1_.py
@@ -25,10 +25,32 @@ def serializar():
     #  { "prenda": "remeras", "cantidad": 12 }
     # Que su lista de prendas dentro del JSON tenga al menos 2 prendas
 
-    # json_data = {...}
+    json_data = {
+                "Nombre" : "Alejandro",
+                "Apellido" : "Cardona",
+                "DNI" : "98.765.432",
+                "Guardaropa" : [
+                    {
+                        "prenda" : "zapatillas",
+                        "cantidad" : 4
+                    },
+                    {
+                        "prenda" : "remeras",
+                        "cantidad" : 12
+                    },{
+                        "prenda" : "pantalones",
+                        "cantidad" : 6
+                    }
+                    ]
+
+                }
 
     # Una vez que finalice el JSON realice un "dump" para almacenarlo en
     # un archivo que usted defina
+
+    with open('ale.json', 'w') as jsonfile:
+        data = [json_data]
+        json.dump(data, jsonfile, indent=4)
 
     # Observe el archivo y verifique que se almaceno lo deseado
 
@@ -40,10 +62,15 @@ def deserializar():
     # del archivo y guardarlo en un objeto JSON utilizando el método
     # load()
 
+    with open('ale.json', 'r') as jsonfile:
+        json_data2 = json.load(jsonfile)
+
     # Luego debe convertir ese JSON data en json_string utilizando
     # el método "dumps" y finalmente imprimir en pantalla el resultado
     # Recuerde utilizar indent=4 para poder observar mejor el resultado
     # en pantalla y comparelo contra el JSON que generó en la función anterior
+
+    print(json.dumps(json_data2, indent=4))
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
